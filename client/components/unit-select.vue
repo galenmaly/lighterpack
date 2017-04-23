24e104e0ef1318b4030bf79817a483dc7223f4e8
galenmaly-ubuntu1 <galenmaly@gmail.com>
2017-04-23 01:14:10 -0700

    Improve unit select UX

--- client/components/unit-select.vue
+++ client/components/unit-select.vue
@@ -3,10 +3,13 @@
 </style>
 
 <template>
-    <div class="lpUnitSelect" :class="{lpOpen: isOpen}" v-on:click="toggle">
+    <div class="lpUnitSelect" :class="{lpOpen: isOpen, lpHover: isFocused}" v-on:click="toggle($event)">
+        <select class="lpUnit lpInvisible" :value="unit" v-on:keyup="keyup($event)" v-on:focus='focusSelect' v-on:blur='blurSelect'>
+            <option v-for="unit in units" :value="unit">{{unit}}</option>
+        </select>
         <span class="lpDisplay">{{unit}}</span>
         <i class="lpSprite lpExpand"></i>
-        <ul class="lpUnitDropdown">
+        <ul :class="'lpUnitDropdown ' + unit">
             <li v-for="unit in units" :class="unit" v-on:click="select(unit)">{{unit}}</li>
         </ul>
     </div>
@@ -24,17 +27,58 @@ export default {
                 "g",
                 "kg"
             ],
-            isOpen: false
+            isOpen: false,
+            isFocused: false
         };
     },
     methods: {
-        toggle: function() {
-            this.isOpen = !this.isOpen;
+        toggle: function(evt) {
+            evt.stopPropagation();
+            if (!this.isOpen) {
+                this.open();
+            } else {
+                this.close();
+            }
+        },
+        open: function() {
+            this.isOpen = true;
+            this.bindCloseListeners();
+        },
+        close: function() {
+            this.isOpen = false;
+            this.unbindCloseListeners();
         },
         select: function(unit) {
             if (typeof this.onChange === "function") {
                 this.onChange(unit);
             }
+        },
+        keyup: function(evt) {
+            if (typeof this.onChange === "function") {
+                this.onChange(evt.target.value);
+            }
+        },
+        bindCloseListeners: function() {
+            window.addEventListener('keyup', this.closeOnEscape);
+            window.addEventListener('click', this.closeOnClick);
+        },
+        unbindCloseListeners: function() {
+            window.removeEventListener('keyup', this.closeOnEscape);
+            window.removeEventListener('click', this.closeOnClick);
+        },
+        closeOnEscape: function(evt) {
+            if (evt.keyCode === 27) {
+                this.close();
+            }
+        },
+        closeOnClick: function(evt) {
+            this.close();
+        },
+        focusSelect: function() {
+            this.isFocused = true;
+        },
+        blurSelect: function() {
+            this.isFocused = false;
         }
     }
 }
