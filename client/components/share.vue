2117d138a77a707c8a3181ba6f4d77f6d5a3b11e
Galen Maly <galenmaly@gmail.com>
2019-05-07 12:34:59 -0700

    Style the fields in the share box

--- client/components/share.vue
+++ client/components/share.vue
@@ -1,16 +1,22 @@
 <style lang="scss">
-
+#share label {
+    font-weight: bold;
+}
 </style>
 
 <template>
     <span v-if="isSignedIn" class="headerItem hasFlyout">
         <span id="share" class="lpFlyout" @mouseenter="focusShare">
             <span class="lpTarget"><i class="lpSprite lpLink"></i> Share</span>
-            <div class="lpContent">
-                <h3>Share your list</h3>
-                <input type="text" id="shareUrl" :value="shareUrl" v-select-on-bus="'show-share-box'"/>
-                <h3>Embed your list</h3>
-                <textarea id="embedUrl" v-select-on-focus>&lt;script src="{{this.baseUrl}}/e/{{this.externalId}}"&gt;&lt;/script&gt;&lt;div id="{{this.externalId}}"&gt;&lt;/div&gt;</textarea>
+            <div class="lpContent lpFields">
+                <div class="lpField">
+                    <label for="shareUrl">Share your list</label>
+                    <input type="text" id="shareUrl" :value="shareUrl" v-select-on-bus="'show-share-box'"/>
+                </div>
+                <div class="lpField">
+                    <label for="embedUrl">Embed your list</label>
+                    <textarea id="embedUrl" v-select-on-focus>&lt;script src="{{this.baseUrl}}/e/{{this.externalId}}"&gt;&lt;/script&gt;&lt;div id="{{this.externalId}}"&gt;&lt;/div&gt;</textarea>
+                </div>
                 <a :href="csvUrl"  id="csvUrl" target="_blank" class="lpHref"><i class="lpSprite lpSpriteDownload"></i>Export to CSV</a>
             </div>
         </span>
