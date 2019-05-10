8dd94e15379e4c3feb5e6ed0840147f24ea855e5
Galen Maly <galenmaly@gmail.com>
2019-05-10 08:04:43 -0700

    Introduce Popover and PopoverHover components

---
+++ client/components/popover-hover.vue
@@ -0,0 +1,43 @@
+<style lang="scss">
+@import "../css/_globals";
+
+</style>
+
+<template>
+    <Popover :shown="shown" @mouseover.native="show" @mouseout.native="startHideTimeout">
+        <slot slot="target" name="target" />
+        <slot slot="content" name="content" />
+    </Popover>
+</template>
+
+<script>
+import Popover from './popover.vue';
+
+export default {
+    name: 'PopoverHover',
+    components: {
+        Popover,
+    },
+    data() {
+        return {
+            shown: false,
+            hideTimeout: null,
+        };
+    },
+    methods: {
+        show() {
+            if (this.hideTimeout) {
+                clearTimeout(this.hideTimeout);
+                this.hideTimeout = null;
+            }
+            this.shown = true;
+        },
+        startHideTimeout() {
+            this.hideTimeout = setTimeout(this.hide, 50);
+        },
+        hide() {
+            this.shown = false;
+        }
+    },
+};
+</script>
