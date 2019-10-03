b742e7562f62e823a3da83023cf9d069455f7d0f
Galen Maly <galenmaly@gmail.com>
2019-10-03 21:15:16 +0800

    Update footer

--- client/components/blackout-footer.vue
+++ client/components/blackout-footer.vue
@@ -5,12 +5,13 @@
 <template>
     <div id="lpWelcomeFooter">
         <div class="lpSiteBy">
-            Site by <a class="lpHref" href="http://www.galenmaly.com/">Galen Maly</a>
+            Site by <a class="lpHref" href="https://www.galenmaly.com/" target="_blank" rel="noopener noreferrer">Galen Maly</a>
+            and <a class="lpHref" href="https://github.com/galenmaly/lighterpack/graphs/contributors" target="_blank" rel="noopener noreferrer">friends</a>.
         </div>
         <div class="lpContact">
-            Copyleft LighterPack 2017
+            Copyleft LighterPack 2019
             -
-            <a class="lpHref" href="https://github.com/galenmaly/lighterpack">Fork me on GitHub</a>
+            <a class="lpHref" href="https://github.com/galenmaly/lighterpack" target="_blank" rel="noopener noreferrer">Fork me on GitHub</a>
             -
             <a class="lpHref" href="mailto:info@lighterpack.com">Contact</a>
         </div>
