6055a6d01b96e656ebda51017d62882775b0a1dc
Galen Maly <galenmaly@gmail.com>
2019-10-04 16:03:10 +0800

    Refactor signin form to separate component

--- client/views/signin.vue
+++ client/views/signin.vue
@@ -13,28 +13,7 @@
                     Need to register?
                 </router-link>
             </div>
-
-            <form class="signin" @submit.prevent="signin">
-                <p v-if="message" class="lpSuccess">
-                    {{ message }}
-                </p>
-                <div class="lpFields">
-                    <input v-model="username" v-focus-on-create type="text" placeholder="Username" name="username" class="username">
-                    <input v-model="password" v-select-on-bus="'focus-signin-password'" type="password" placeholder="Password" name="password" class="password">
-                </div>
-
-                <errors :errors="errors" />
-
-                <div class="lpButtons">
-                    <button class="lpButton">
-                        Sign in
-                        <spinner v-if="fetching" />
-                    </button>
-                    <router-link to="/forgot-password" class="lpHref alternateAction">
-                        Forgot username/password?
-                    </router-link>
-                </div>
-            </form>
+            <SigninForm :message="message" />
         </modal>
 
         <blackoutFooter />
@@ -43,73 +22,24 @@
 
 <script>
 import blackoutFooter from '../components/blackout-footer.vue';
-import errors from '../components/errors.vue';
 import modal from '../components/modal.vue';
-import spinner from '../components/spinner.vue';
+import SigninForm from '../components/signin-form.vue';
 
 export default {
-    name: 'Welcome',
+    name: 'Signin',
     components: {
         blackoutFooter,
-        errors,
+        globalAlerts,
         modal,
-        spinner,
+        SigninForm,
     },
-    data() {
-        return {
-            fetching: false,
-            errors: [],
-            username: '',
-            password: '',
-            message: '',
-        };
-    },
-    beforeMount() {
-        if (this.$route.path.indexOf('/reset-password') > -1 || this.$route.path.indexOf('/forgot-username') > -1) {
-            this.message = 'An email has been sent to the address associated with your account.';
+    computed: {
+        message() {
+            if (this.$route.path.indexOf('/reset-password') > -1 || this.$route.path.indexOf('/forgot-username') > -1) {
+               return 'An email has been sent to the address associated with your account.';
+            }
+            return '';
         }
-    },
-    methods: {
-        signin() {
-            this.errors = [];
-
-            if (!this.username) {
-                this.errors.push({ field: 'username', message: 'Please enter a username.' });
-            }
-
-            if (!this.password) {
-                this.errors.push({ field: 'password', message: 'Please enter a password.' });
-            }
-
-            if (this.errors.length) {
-                return;
-            }
-
-            this.fetching = true; // ho ho
-
-            return fetchJson('/signin/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                credentials: 'same-origin',
-                body: JSON.stringify({ username: this.username, password: this.password }),
-            })
-                .then((response) => {
-                    this.$store.commit('setSyncToken', response.syncToken);
-                    this.$store.commit('loadLibraryData', response.library);
-                    this.$store.commit('setSaveType', 'remote');
-                    this.$store.commit('setLoggedIn', response.username);
-                    this.$router.push('/');
-                    this.fetching = false;
-                })
-                .catch((err) => {
-                    this.errors = err;
-                    bus.$emit('focus-signin-password');
-                    this.password = '';
-                    this.fetching = false;
-                });
-        },
     },
 };
 </script>
